edee01601ceda1ce05a926e6561d6828b96a67e7
Daniel <dsjsubx2.4c@gmail.om>
2024-09-08 12:32:31 -0600

    First Commit to this app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mi_app/Controller/notifier.dart';
+import 'package:mi_app/View/components/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
